3a905b8ca6ec1e5fdca49a7fe3322b4b9cc1d567
ajames72 <andrew.james@ajmebc.co.uk>
2017-06-15 17:18:21 +0100

    Added margin to homepage
    Added margin to homepage

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HomePage = () => {
   return (
-    <div className="jumbotron">
+    <div className="homepage jumbotron">
       <h1>The Movie Database</h1>
       <p>React, Redux and React Router in ES6</p>
     </div>
